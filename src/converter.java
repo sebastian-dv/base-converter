387097b7d9517671ebf8e6cb811d544aaa7e7040
Sebastian <sebastiandiaz1222@gmail.com>
2023-07-10 12:29:39 -0700

    added new global vars

--- src/converter.java
+++ src/converter.java
@@ -4,6 +4,7 @@ public class converter {
     private String number;
     private String result;
     private static final int MAX_BASE = 36;
+    private static final int MIN_BASE = 2;
     
     public converter() {
         this.baseFrom = 0;
@@ -13,7 +14,7 @@ public class converter {
     }
 
     public void start() {
-        
+
     }
 
     public void setBaseFrom(int baseFrom) {
